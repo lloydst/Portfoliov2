0441ee32868bc07f211cd40235da7995549ac805
lloydst <lloyd.stumpel@gmail.com>
2017-11-23 15:33:00 +0100

    getting ready for heroku

--- server/app.js
+++ server/app.js
@@ -72,7 +72,7 @@ app.get('*', (req, res) => {
 });
 
 // Server
-app.set( 'port', ( process.env.PORT || 5000 ));
+app.set( 'port', ( process.env.port || 5000 ));
 
 // Start node server
 app.listen( app.get( 'port' ), function() {
