1fe724b6d0120a655dbff3c6051e6945d5a8f096
lloydst <lloyd.stumpel@gmail.com>
2017-12-07 10:24:26 +0100

    styling and more placeholder content added

--- src/app/internships/internships.component.ts
+++ src/app/internships/internships.component.ts
@@ -10,7 +10,10 @@ internships = [
   {
     company: 'Poort80',
     website: 'poort80.nl',
-    duration: '19-7-2017 tot 19-12-2017'
+    duration: '19-7-2017 - 19-12-2017',
+    description: 'Poort80 is a webdevelopment company based in the netherlands. '
+    + '<br> They use the mean stack (mongodb, express, angular, nodejs). as well as other languages'
+    + '<h1>to do more text.</h1>'
   }
   // {
   //   company: '',
